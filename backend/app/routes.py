4ecbb83688d4ba28c5c2ffa3068c2deaa0fa50d4
Jobin Tom <92356462+jobint001@users.noreply.github.com>
2024-06-29 11:06:17 +0530

    changes to gitignore

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,40 @@
 from flask import Blueprint, request, jsonify
 from app.watson import get_access_token, analyze_text
+import requests
 
 main = Blueprint('main', __name__)
+parameters = {
+    "decoding_method": "greedy",""
+    "max_new_tokens": 300,
+    "min_new_tokens": 50,
+    "repetition_penalty": 1
+}
+
+model_id = "ibm/granite-13b-chat-v2"
+
+class Prompt:
+    def __init__(self, access_token, project_id):
+        self.access_token = access_token
+        self.project_id = project_id
+
+    def generate(self, input, model_id, parameters):
+        wml_url = "https://us-south.ml.cloud.ibm.com/ml/v1-beta/generation/text?version=2023-05-28"
+        Headers = {
+            "Authorization": "Bearer " + self.access_token,
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        data = {
+            "model_id": model_id,
+            "input": input,
+            "parameters": parameters,
+            "project_id": self.project_id
+        }
+        response = requests.post(wml_url, json=data, headers=Headers)
+        if response.status_code == 200:
+            return response.json()["results"][0]["generated_text"]
+        else:
+            return response.text
 
 @main.route('/')
 def index():
@@ -11,11 +44,7 @@ def index():
 def analyze_feedback():
     data = request.get_json()
     
-
-    
-
     access_token = get_access_token()
-    print(access_token)
     analysis = analyze_text("text", access_token)
 
     return jsonify(analysis)
